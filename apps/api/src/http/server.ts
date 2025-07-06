90fc775b76a1d4f581c85e051a7268529fb97932
souorichard <rodrigues.lpta@gmail.com>
2025-07-05 21:13:44 -0300

    feat(api): add Swagger setup

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,10 +1,15 @@
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+
+import { createAccount } from './routes/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -15,7 +20,25 @@ app.register(fastifyCors)
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+// Swagger
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Controlizze API',
+      description: 'Full-stack SaaS app with multi-tenant architecture & RBAC.',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
 // Routes
+app.register(createAccount)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
