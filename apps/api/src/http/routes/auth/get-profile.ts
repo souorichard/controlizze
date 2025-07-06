56eee10ab3f0ed0df4f63a6bfad33c4fcd4474fd
souorichard <rodrigues.lpta@gmail.com>
2025-07-06 20:34:11 -0300

    feat(api): add auth middleware

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -2,48 +2,52 @@ import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod/v4'
 
+import { auth } from '@/http/middlewares/auth'
 import { NotFoundError } from '@/http/routes/_errors/not-found-error'
 import { prisma } from '@/lib/prisma'
 
 export async function getProfile(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/profile',
-    {
-      schema: {
-        tags: ['Auth'],
-        summary: 'Get authenticated user profile.',
-        response: {
-          200: z.object({
-            user: z.object({
-              id: z.uuid(),
-              name: z.string().nullable(),
-              email: z.email(),
-              avatarUrl: z.string().nullable(),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/profile',
+      {
+        schema: {
+          tags: ['Auth'],
+          summary: 'Get authenticated user profile.',
+          response: {
+            200: z.object({
+              user: z.object({
+                id: z.uuid(),
+                name: z.string().nullable(),
+                email: z.email(),
+                avatarUrl: z.string().nullable(),
+              }),
             }),
-          }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { sub } = await request.jwtVerify<{ sub: string }>()
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
 
-      const user = await prisma.user.findUnique({
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          avatarUrl: true,
-        },
-        where: {
-          id: sub,
-        },
-      })
+        const user = await prisma.user.findUnique({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUrl: true,
+          },
+          where: {
+            id: userId,
+          },
+        })
 
-      if (!user) {
-        throw new NotFoundError('User not found.')
-      }
+        if (!user) {
+          throw new NotFoundError('User not found.')
+        }
 
-      return reply.status(200).send({ user })
-    },
-  )
+        return reply.status(200).send({ user })
+      },
+    )
 }
