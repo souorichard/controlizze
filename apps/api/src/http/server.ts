31ba24746c9f204a5e77f47ab7cae409a8263279
souorichard <rodrigues.lpta@gmail.com>
2025-07-10 21:41:44 -0300

    feat(api): add get members route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { getMembers } from './routes/members/get-members'
 import { createOrganization } from './routes/organization/create-organization'
 import { getMembership } from './routes/organization/get-membership'
 import { getOrganization } from './routes/organization/get-organization'
@@ -96,6 +97,8 @@ app.register(getTransation)
 app.register(updateTransation)
 app.register(deleteTransation)
 app.register(changeTransationStatus)
+
+app.register(getMembers)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
