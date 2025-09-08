70b7af13b30f1ef7875dc049de7b69c93b368d7d
souorichard <rodrigues.lpta@gmail.com>
2025-09-08 12:08:59 -0300

    feat(web): add OrganizationAvatarForm base layout and integrate into settings page

--- apps/web/src/app/(app)/organizations/[slug]/settings/_components/settings-nav-link.tsx
+++ apps/web/src/app/(app)/organizations/[slug]/settings/_components/settings-nav-link.tsx
@@ -14,7 +14,7 @@ export function SettingsNavLink(props: SettingsNavLinkProps) {
   return (
     <Link
       data-current={isCurrent}
-      className="text-muted-foreground data-[current=true]:bg-secondary/40 data-[current=true]:text-foreground data-[current=false]:hover:text-foreground flex w-full items-center gap-2 rounded-md px-4 py-2 text-sm font-medium transition"
+      className="text-muted-foreground data-[current=true]:bg-secondary/40 data-[current=true]:text-foreground data-[current=false]:hover:text-foreground flex w-full items-center justify-center gap-2 rounded-md px-4 py-2 text-sm font-medium transition lg:justify-start"
       {...props}
     >
       {props.children}
