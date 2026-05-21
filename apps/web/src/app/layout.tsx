a86494253df02b81ef7a07a8ed4327e0056aabf3
souorichard <rodrigues.lpta@gmail.com>
2026-05-21 20:31:54 -0300

    feat(web): add initial Nex.jst setup with tailwind and shadcn

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import { Geist, Space_Grotesk } from 'next/font/google'
+import './globals.css'
+
+const spaceGroteskSans = Space_Grotesk({
+  variable: '--font-space-grotesk-sans',
+  subsets: ['latin'],
+})
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Controlizze',
+    template: '%s | Controlizze',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${spaceGroteskSans.variable} ${geistSans.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  )
+}
